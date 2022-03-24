a9dbcce7319b0589894706e4d6f72324212de188
tanverrana <tanversobur@gmail.com>
2022-03-25 01:38:10 +0600

    show products with button

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -6,7 +6,16 @@ const Products = (props) => {
     return (
         <div className="product">
             <img src={img} alt=""></img>
-            <p>{name}</p>
+            <div className="product-info">
+                <p className="product-name">{name}</p>
+                <p>Price:{price}</p>
+                <p><small>Seller:{seller}</small></p>
+                <p><small>Ratings:{ratings} stars</small></p>
+            </div>
+            <button className="btn-cart">
+                <p>Add To Cart</p>
+
+            </button>
         </div>
     );
 };
