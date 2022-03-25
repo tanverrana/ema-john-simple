11d35710ecf8aab27c42d275c7a24223b35dca96
tanverrana <tanversobur@gmail.com>
2022-03-25 14:55:19 +0600

    products added

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -29,13 +29,24 @@ const Shop = () => {
         }
         setCart(savedCart);
         //console.log("local storage finished");
-    }, [products])
+    }, [products]);
 
-    const handleAddToCart = (product) => {
-        //console.log(product);
-        const newCart = [...cart, product];
+    const handleAddToCart = (selectedProduct) => {
+        console.log(selectedProduct);
+        let newCart = [];
+        const exists = cart.find(product => product.id === selectedProduct.id);
+        if (!exists) {
+            selectedProduct.quantity = 1;
+            newCart = [...cart, selectedProduct];
+        }
+        else {
+            const rest = cart.filter(product => product.id !== selectedProduct.id);
+            exists.quantity = exists.quantity + 1;
+            newCart = [...rest, exists]
+        }
+        //const newCart = [...cart, selectedProduct];
         setCart(newCart);
-        addToDb(product.id)
+        addToDb(selectedProduct.id)
     }
     return (
         <div className="shop-container">
