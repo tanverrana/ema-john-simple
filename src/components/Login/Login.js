4887490339c3c63cbb29bdd0f4bb6d9e9f890d5f
tanverrana <tanversobur@gmail.com>
2022-04-14 12:13:34 +0600

    login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,22 +1,58 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { Link, useNavigate } from 'react-router-dom';
+import auth from '../../firebase.init';
 import "./Login.css";
 
 const Login = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useSignInWithEmailAndPassword(auth);
+
+    const navigate = useNavigate();
+
+
+    const handleEmailBlur = event => {
+        setEmail(event.target.value);
+    }
+
+    const handlePasswordBlur = event => {
+        setPassword(event.target.value);
+    }
+    if (user) {
+        navigate("/shop");
+    }
+
+    const handleUserSignIn = event => {
+        event.preventDefault();
+
+        signInWithEmailAndPassword(email, password);
+    }
     return (
         <div className="form-container">
             <div>
                 <h2 className="from-title">Login</h2>
-                <form action="">
+                <form onSubmit={handleUserSignIn}>
                     <div className="input-group">
                         <label htmlFor="email">Email</label>
-                        <input type="email" name="email" required />
+                        <input onBlur={handleEmailBlur} type="email" name="email" required />
                     </div>
                     <div className="input-group">
                         <label htmlFor="password">Password</label>
                         <input type="password" name="password" required />
                     </div>
-                    <input className="form-submit" type="submit" value="Login" />
+                    <p style={{ color: "red" }}>{error?.message}</p>
+                    {
+                        loading && <p>Loading...</p>
+                    }
+                    <input onBlur={handlePasswordBlur} className="form-submit" type="submit" value="Login" />
                 </form>
                 <p>New To Ema-John? <Link className="form-link" to="/signup">Create an account</Link></p>
             </div>
