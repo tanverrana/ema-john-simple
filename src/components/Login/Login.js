a2fb065107d3e1a5cdc1b0bf4e17ba25cc181ab2
tanverrana <tanversobur@gmail.com>
2022-04-14 09:50:22 +0600

    form style

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import "./Login.css";
 
 const Login = () => {
     return (
-        <div>
-            <h2>This is login</h2>
+        <div className="form-container">
+            <div>
+                <h2 className="from-title">Login</h2>
+                <form action="">
+                    <div className="input-group">
+                        <label htmlFor="email">Email</label>
+                        <input type="email" name="email" />
+                    </div>
+                    <div className="input-group">
+                        <label htmlFor="password">Password</label>
+                        <input type="password" name="password" />
+                    </div>
+                    <input className="from-submit" type="submit" value="Login" />
+                </form>
+                <p>New To Ema-John? <Link className="form-link" to="/signup">Create an account</Link></p>
+            </div>
         </div>
     );
 };
